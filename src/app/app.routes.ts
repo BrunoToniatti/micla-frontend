3527040ea35c47e0583333f4b331d47c61081ef5
Bruno Toniatti <toniattibruno08@gmail.com>
2025-07-02 15:11:49 -0300

    Adicionando users em navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './layout/layout.component';
+import { UsersComponent } from './pages/users/users.component';
 
 export const routes: Routes = [
   {
@@ -20,6 +21,11 @@ export const routes: Routes = [
         path: 'roles',
         loadComponent: () =>
           import('./pages/roles/roles.component').then(r => r.RolesComponent)
+      },
+      {
+        path: 'users',
+        loadComponent: () =>
+          import('./pages/users/users.component').then(u => UsersComponent)
       }
     ]
   },
