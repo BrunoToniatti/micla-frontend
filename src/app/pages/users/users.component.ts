c31cbc14cfad8fd0abf0ddae757a4560ea06fbfe
Bruno Toniatti <toniattibruno08@gmail.com>
2025-07-02 15:37:25 -0300

    Criando layout de usuários

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
-
+users = [
+    { name: 'João Silva', email: 'joao@email.com', role: 'Administrador' },
+    { name: 'Ana Costa', email: 'ana@email.com', role: 'Colaborador' },
+    { name: 'Carlos Lima', email: 'carlos@email.com', role: 'Supervisor' }
+  ];
 }
