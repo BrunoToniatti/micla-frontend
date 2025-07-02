8eb1f34f53365c532e64f53e1512268300f405c6
Bruno Toniatti <toniattibruno08@gmail.com>
2025-07-02 15:43:56 -0300

    Criando services de users

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  apiUrl = environment.apiUrl + 'accounts/user/';
+
+  getUsers() {
+    return this.http.get(`${this.apiUrl}`)
+  }
+}
