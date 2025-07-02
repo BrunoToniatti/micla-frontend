ec6c03cbd3c2be8db70f87602ac381d4094c56bf
Bruno Toniatti <toniattibruno08@gmail.com>
2025-07-02 18:04:58 -0300

    Criando form de users

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,19 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { UsersService } from '../../services/users.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
 
+  cargos = ['Administrador', 'Colaborador', 'Supervisor'];
+
+  showForm = false;
   users: any = []
+
+  newUser = {
+    email: '',
+    role: '',
+  }
 
   constructor(
     private userService: UsersService
@@ -21,6 +31,14 @@ export class UsersComponent {
 
   ngOnInit(): void{
     this.listUsers()
+  }
+
+  toggleForm(){
+    this.showForm = !this.showForm
+  }
+
+  addUser(){
+
   }
 
   listUsers(){
